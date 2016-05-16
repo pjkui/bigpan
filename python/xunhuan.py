150fbbd316df449d256cf2ec45ee9f17a5076ba8
Wujunze <admin@wujunze.com>
2016-05-16 16:36:50 +0800

    python中的字典

--- python/xunhuan.py
+++ python/xunhuan.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-lang_list = ['php','python','java','C','C#','JavaScript']
-x = 0
-y = 999
-while y > 0 :
-    x = x + y
-    y = y-2
-print(x)
+my_dict = {'php': 1,'java': 2,'python':3}
+my_dict['ruby'] = 6
+print(my_dict)
