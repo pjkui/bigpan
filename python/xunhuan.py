64c133281caa2b602426571ba8a6dc5a72861b32
Wujunze <admin@wujunze.com>
2016-05-16 16:16:55 +0800

    高斯的数学

--- python/xunhuan.py
+++ python/xunhuan.py
@@ -4,6 +4,9 @@
 lang_list = ['php','python','java','C','C#','JavaScript']
 x = 0
 num_list = [1,2,3,4,5,6,7]
+num_list = list(range(101))
+print(type(num_list))
+
 for num in num_list:
     x = x+num
 print(x)
