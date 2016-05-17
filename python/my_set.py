c69097274656d0a97f377ee40e6f60f3dc4a3d89
Wujunze <admin@wujunze.com>
2016-05-17 19:13:23 +0800

    set的运算

--- python/my_set.py
+++ python/my_set.py
@@ -6,4 +6,10 @@ my_set = set(['1',2,3,4,2,1,'1'])
 my_set.add(6)
 #set删除一个元素
 my_set.remove(3)
-print(my_set)
+#print(my_set)
+set1 = set([1,2,3,4])
+set2 = set([3,4,5,6])
+#取两个set的交集
+print(set1 & set2)
+#取两个set的并集
+print(set1 | set2)
