89c23813d206db57ac9594eeb0663e5158f30c77
Wujunze <admin@wujunze.com>
2016-05-20 15:23:21 +0800

    关键字参数给个默认值  就可以不传这个参数

--- python/two/func.py
+++ python/two/func.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #限制关键字参数的名字
-def reg (name,sex,*,city,domain):
+def reg (name,sex,*,city='上海',domain):
     print('姓名:',name,'性别:',sex,'其他信息:','city',city,'domain',domain)
 
-reg('小米','男',city='昆明',domain='www.google.com')
+reg('小米','男',domain='www.google.com')
