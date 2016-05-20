d4b39d4c2439cf6377aea12b57f44d828c31fbaa
Wujunze <admin@wujunze.com>
2016-05-20 14:53:36 +0800

    函数的参数为一个list或者tuple

--- python/two/func.py
+++ python/two/func.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-def add_end (info = None):
-    if info is None:
-        info = []
-    info.append('END')
-    return info
+def add_end (infos):
+    num =0
+    for ss in infos:
+        num = num + ss * ss
+    return num
 #第一次调用
-r = add_end()
-#第二次调用
-r = add_end()
-#第三次调用
-r = add_end()
+r = add_end([1,5,6])
 print(r)
 
 # Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
