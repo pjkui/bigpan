2c22f25811d09f214ff6601ea2b60d676342b225
Wujunze <admin@wujunze.com>
2016-05-20 14:32:38 +0800

    定义默认参数的注意事项

--- python/two/func.py
+++ python/two/func.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-def my_func(num):
-    if num > 18:
-        print('成年人年龄是 %d' % num)
-    else:
-        print(num)
+def add_end (info = []):
+    info.append('END')
+    return info
+#第一次调用
+r = add_end()
+#第二次调用
+r = add_end()
+#第三次调用
+r = add_end()
+print(r)
+
+# Python函数在定义的时候，默认参数L的值就被计算出来了，即[]，因为默认参数L也是一个变量，它指向对象[]，每次调用该函数，如果改变了L的内容，则下次调用时，默认参数的内容就变了，不再是函数定义时的[]了。
+#
+# 所以，定义默认参数要牢记一点：默认参数必须指向不变对象！
