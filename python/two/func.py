6a9d8c886fac74c3712f6cbeb58000a6c4512a40
Wujunze <admin@wujunze.com>
2016-05-20 15:11:02 +0800

    直接把字典传进一个**kv 的可变参数    会报错

--- python/two/func.py
+++ python/two/func.py
@@ -3,5 +3,13 @@
 #定义一个关键字参数的函数    函数内部自动把关键字参数组装成dict
 def reg (name,sex,**infos):
     print('姓名:',name,'性别:',sex,'其他信息:',infos)
+map = {'lamg':'C++','IDC':'阿里云','domain':'云'}
 
-reg('小米','男',city='北京',inc='奇虎')
+reg('小米','男',map)
+
+# 如果直接传一个dict进函数   会报错
+
+# Traceback (most recent call last):
+#   File "D:/pystudy/study/bigpan/python/two/func.py", line 7, in <module>
+#     reg('小米','男',map)
+# TypeError: reg() takes 2 positional arguments but 3 were given
