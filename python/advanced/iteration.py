0897eba4eb9e05b60294d4b155b97f5adc451207
wujunze <1017109588@qq.com>
2016-06-13 17:02:36 +0800

    判断一个对象是否是可迭代对象

--- python/advanced/iteration.py
+++ python/advanced/iteration.py
@@ -3,9 +3,12 @@
 #迭代
 #for x in [1, 2, 3, 4, 5]:
     #print(x)
-
+from collections import Iterable
 dic = {'name':'xm','city':'qingdao','sex':'nan'}
 dir_two = {'dff':90,'hh':78,'bh':67}
+#判断一个对象是否是可迭代对象
+print(isinstance(dir_two,Iterable))
+exit('end')
 #迭代字典中的key value
 for X in dic.items():
     print(X)
