4a2cf3d5fa7022c719f2b46be88222ead13a0658
wujunze <1017109588@qq.com>
2016-06-13 16:52:03 +0800

    迭代字典中的key

--- python/advanced/iteration.py
+++ python/advanced/iteration.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#迭代
+#for x in [1, 2, 3, 4, 5]:
+    #print(x)
+
+dic = {'name':'xm','city':'qingdao','sex':'nan'}
+dir_two = {'dff':90,'hh':78,'bh':67}
+#迭代字典中的key
+for X in dic:
+    print(X)
