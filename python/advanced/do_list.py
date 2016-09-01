5cc43b5ec8fc9332ff794396f3843d45a91e860e
wujunze <1017109588@qq.com>
2016-09-01 19:32:12 +0800

    显示当前目录下的所有文件

--- python/advanced/do_list.py
+++ python/advanced/do_list.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 #列表生成式
 #L = [ p * p for p in range(1,99)]
 #print(L)
-#for k in L:
- #   print(k)
-
+# for k in L:
+#   print(k)
+'''
 #两层循环，可以生成全排列
 N = ['Tom','Jack','Jim','Jason']
 A = ['do','sleep','eat','dance']
 P = [ name + action for name in N for action in A]
 
 print(P)
+ '''
+#查看当前目录下的所有文件
+print([d for d in os.listdir('../')])
+
